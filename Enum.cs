b07db42da2ee13dfebe747286026b40969b623e4
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:52:46 +0400

    Create Enum.cs

---
+++ Enum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetGame
+{
+   enum Animals
+    {
+        None,
+        Dragon,
+        Hydra,
+        Basilisk,
+        Chameleon,
+        Unicorn,
+        Manticore,
+        Hippogriff,
+        Phoenix,
+        Leprechaun
+    }
+
+   enum Actions
+    {
+        None,
+        Feed,
+        Drink,
+        Clean,
+        Play,
+        Sleep,
+        Fight,
+        Hug,
+        Abandon
+    }
+
+}
